d7fa33a05cb94366645311fa7b172415626dfe0e
laurent payen <laurent.payen@gmail.com>
2018-06-07 11:25:25 +0200

    WFAPPOINT-23 : Send an ical to the user associated to the appointment

--- src/java/fr/paris/lutece/plugins/workflow/modules/appointment/web/UpdateAdminAppointmentTaskComponent.java
+++ src/java/fr/paris/lutece/plugins/workflow/modules/appointment/web/UpdateAdminAppointmentTaskComponent.java
@@ -33,23 +33,37 @@
  */
 package fr.paris.lutece.plugins.workflow.modules.appointment.web;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.StringJoiner;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
+import fr.paris.lutece.plugins.appointment.business.appointment.Appointment;
+import fr.paris.lutece.plugins.appointment.business.form.Form;
+import fr.paris.lutece.plugins.appointment.business.localization.Localization;
+import fr.paris.lutece.plugins.appointment.business.slot.Slot;
+import fr.paris.lutece.plugins.appointment.business.user.User;
+import fr.paris.lutece.plugins.appointment.service.AppointmentService;
+import fr.paris.lutece.plugins.appointment.service.FormService;
+import fr.paris.lutece.plugins.appointment.service.LocalizationService;
+import fr.paris.lutece.plugins.appointment.service.SlotService;
+import fr.paris.lutece.plugins.appointment.service.UserService;
 import fr.paris.lutece.plugins.workflow.modules.appointment.business.UpdateAdminAppointmentHistory;
 import fr.paris.lutece.plugins.workflow.modules.appointment.business.UpdateAdminAppointmentHistoryHome;
+import fr.paris.lutece.plugins.workflow.modules.appointment.service.ICalService;
 import fr.paris.lutece.plugins.workflow.web.task.NoConfigTaskComponent;
 import fr.paris.lutece.plugins.workflowcore.service.task.ITask;
 import fr.paris.lutece.portal.business.user.AdminUser;
 import fr.paris.lutece.portal.business.user.AdminUserHome;
 import fr.paris.lutece.portal.service.i18n.I18nService;
+import fr.paris.lutece.portal.service.mail.MailService;
 import fr.paris.lutece.portal.service.message.AdminMessage;
 import fr.paris.lutece.portal.service.message.AdminMessageService;
 import fr.paris.lutece.portal.service.template.AppTemplateService;
@@ -68,14 +82,22 @@ public class UpdateAdminAppointmentTaskComponent extends NoConfigTaskComponent
     private static final String MESSAGE_MANDATORY_FIELD = "portal.util.message.mandatoryFields";
     private static final String MESSAGE_ADMIN_USER_ASSOCIATED_TO_APPOINTMENT = "module.workflow.appointment.task_update_admin_appointment_config.adminUserAssociatedToAppointment";
 
+    private static final String DATE_OF_APPOINTMENT = "module.workflow.appointment.task_notify_appointment_config.label_date_appointment";
+    private static final String TIME_OF_APPOINTMENT = "module.workflow.appointment.task_notify_appointment_config.label_time_appointment";
+    private static final String LAST_NAME_OF_USER = "module.workflow.appointment.task_notify_appointment_config.label_lastname";
+    private static final String FIRST_NAME_OF_USER = "module.workflow.appointment.task_notify_appointment_config.label_firstname";
+    private static final String APPOINTMENT_WITH = "module.workflow.appointment.task_notify_appointment_config.appointment_with";
     // MARKS
     private static final String MARK_LIST_ADMIN_USERS = "list_admin_users";
 
     // PARAMETERS
     private static final String PARAMETER_ID_ADMIN_USER = "id_admin_user";
-
+    private static final String PARAMETER_IS_CAL_NOTIF = "is_cal_notif";
     // CONSTANTS
     private static final String CONSTANT_SPACE = " ";
+    private static final String COLON = ":";
+
+    public static final String FORMAT_DATE = "dd/MM/yyyy";
 
     /**
      * {@inheritDoc}
@@ -121,51 +143,11 @@ public class UpdateAdminAppointmentTaskComponent extends NoConfigTaskComponent
             return AdminMessageService.getMessageUrl( request, MESSAGE_MANDATORY_FIELD, AdminMessage.TYPE_STOP );
         }
 
-        // Appointment appointment = AppointmentService.findAppointmentById(nIdResource);
-        // Slot slot = SlotService.findSlotById(appointment.getIdSlot());
-        // AppointmentFilter filter = new AppointmentFilter();
-        // filter.setDateAppointment(appointment.getDateAppointment());
-        //
-        // List<Appointment> listAppointments = AppointmentHome.getAppointmentListByFilter(filter);
-        //
-        // if (listAppointments.size() > 0) {
-        // Slot slot = SlotService.findSlotById(appointment.getIdSlot());
-        // AppointmentSlot slotOfMatchingAppointment = null;
-        //
-        // for (Appointment appointmentFound : listAppointments) {
-        // if ((appointmentFound.getStatus() != Appointment.Status.STATUS_UNRESERVED.getValeur())
-        // && (appointment.getIdAppointment() != appointmentFound.getIdAppointment())) {
-        // if (appointment.getIdSlot() == appointmentFound.getIdSlot()) {
-        // slotOfMatchingAppointment = slot;
-        //
-        // break;
-        // }
-        //
-        // AppointmentSlot slotFound = AppointmentSlotHome.findByPrimaryKey(appointmentFound.getIdSlot());
-        //
-        // if (slot.getIdForm() != slotFound.getIdDay()) {
-        // int nTimeStart = (slot.getStartingHour() * 100) + slot.getStartingMinute();
-        // int nTimeEnd = (slot.getEndingHour() * 100) + slot.getEndingMinute();
-        // int nTimeStartFound = (slotFound.getStartingHour() * 100) + slotFound.getStartingMinute();
-        // int nTimeEndFound = (slotFound.getEndingHour() * 100) + slotFound.getEndingMinute();
-        //
-        // if (((nTimeStartFound > nTimeStart) && (nTimeStartFound < nTimeEnd))
-        // || ((nTimeStart > nTimeStartFound) && (nTimeStart < nTimeEndFound))) {
-        // slotOfMatchingAppointment = slotFound;
-        //
-        // break;
-        // }
-        // }
-        // }
-        // }
-        //
-        // if (slotOfMatchingAppointment != null) {
-        // AppointmentForm form = AppointmentFormHome.findByPrimaryKey(slotOfMatchingAppointment.getIdForm());
-        // Object[] args = { form.getTitle() };
-        // AdminMessageService.getMessageUrl(request, ERROR_MESSAGE_ADMIN_USER_BUSY, args, AdminMessage.TYPE_STOP);
-        // }
-        // }
-
+        String strIsCalNotif = request.getParameter( PARAMETER_IS_CAL_NOTIF );
+        if ( Boolean.parseBoolean( strIsCalNotif ) )
+        {
+            sendICal( nIdResource, adminUser, locale );
+        }
         return null;
     }
 
@@ -201,5 +183,41 @@ public class UpdateAdminAppointmentTaskComponent extends NoConfigTaskComponent
     public String getTaskInformationXml( int nIdHistory, HttpServletRequest request, Locale locale, ITask task )
     {
         return null;
+    }
+
+    /**
+     * Send an email with an event attached to
+     * 
+     * @param nIdResource
+     *            the id of the appointment
+     * @param adminUser
+     *            the associated user
+     * @param locale
+     *            the locale
+     */
+    private void sendICal( int nIdResource, AdminUser adminUser, Locale locale )
+    {
+        Appointment appointment = AppointmentService.findAppointmentById( nIdResource );
+        User user = UserService.findUserById( appointment.getIdUser( ) );
+        Slot slot = SlotService.findSlotById( appointment.getIdSlot( ) );
+        Form form = FormService.findFormLightByPrimaryKey( slot.getIdForm( ) );
+        Localization localization = LocalizationService.findLocalizationWithFormId( form.getIdForm( ) );
+        String location = StringUtils.EMPTY;
+        if ( localization != null && StringUtils.isNotEmpty( localization.getAddress( ) ) )
+        {
+            location = localization.getAddress( );
+        }
+        String content = new StringJoiner( StringUtils.EMPTY ).add( I18nService.getLocalizedString( DATE_OF_APPOINTMENT, locale ) ).add( CONSTANT_SPACE )
+                .add( COLON ).add( CONSTANT_SPACE ).add( DateTimeFormatter.ofPattern( FORMAT_DATE ).format( slot.getDate( ) ) ).add( StringUtils.LF )
+                .add( I18nService.getLocalizedString( TIME_OF_APPOINTMENT, locale ) ).add( CONSTANT_SPACE ).add( COLON ).add( CONSTANT_SPACE )
+                .add( slot.getStartingTime( ).toString( ) ).add( StringUtils.LF ).add( I18nService.getLocalizedString( FIRST_NAME_OF_USER, locale ) )
+                .add( CONSTANT_SPACE ).add( COLON ).add( CONSTANT_SPACE ).add( user.getFirstName( ) ).add( StringUtils.LF )
+                .add( I18nService.getLocalizedString( LAST_NAME_OF_USER, locale ) ).add( CONSTANT_SPACE ).add( COLON ).add( CONSTANT_SPACE )
+                .add( user.getLastName( ) ).add( StringUtils.LF ).toString( );
+        String subject = new StringJoiner( StringUtils.EMPTY ).add( APPOINTMENT_WITH ).add( CONSTANT_SPACE ).add( COLON ).add( CONSTANT_SPACE )
+                .add( user.getFirstName( ) ).add( CONSTANT_SPACE ).add( user.getLastName( ) ).toString( );
+        // Send an email with a cal event to this admin user
+        ICalService.getService( ).sendAppointment( adminUser.getEmail( ), StringUtils.EMPTY, subject, content, location, MailService.getNoReplyEmail( ),
+                MailService.getNoReplyEmail( ), appointment, Boolean.TRUE );
     }
 }
